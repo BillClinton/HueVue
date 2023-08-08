2a491d4ad32d9ef939313c2b85de52a5029a8772
Bill Clinton <bclinton@appunto.net>
2023-08-07 22:14:59 -0400

    chore: install pinia, axios

--- src/main.ts
+++ src/main.ts
@@ -5,16 +5,18 @@
  */
 
 // Components
-import App from './App.vue'
+import App from "./App.vue";
 
 // Composables
-import { createApp } from 'vue'
+import { createApp } from "vue";
+import { createPinia } from "pinia";
 
 // Plugins
-import { registerPlugins } from '@/plugins'
+import { registerPlugins } from "@/plugins";
 
-const app = createApp(App)
+const pinia = createPinia();
+const app = createApp(App).use(pinia);
 
-registerPlugins(app)
+registerPlugins(app);
 
-app.mount('#app')
+app.mount("#app");
